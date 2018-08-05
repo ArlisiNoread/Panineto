636afb7bfb15a29ba73f1c7a7903b71cc38eb618
ArlisiNoread <41275777+ArlisiNoread@users.noreply.github.com>
2018-08-05 18:09:02 -0500

    Correciones a la Responsividad Logo
    Modificaciones al archivo efectos.js

--- assets/js/efectos.js
+++ assets/js/efectos.js
@@ -1,18 +1,52 @@
-//var fixed = false;
-  $(document).scroll(function() {
-    if( $(this).scrollTop() > 10 ) {
+/* Efecto Logo Panineto */
+
+
+$(document).ready(function(){
+    
+    if($(window).width() < 768) {
+        console.log("Logo Estático");
+        $('#logo-id').css({
+            'width' :'58px',
+            'left' : '150px',
+            display:'block'
+        });
+    } 
+});
+
+$(window).resize(function(){ 
+
+    if($(window).width() < 768) {
+        console.log("Logo Estático < 768");
+        $('#logo-id').css({
+            'width' :'58px',
+            'left' : '150px',
+            display:'block'
+        });
+    }  else  {
+        console.log("Logo Estático >= 768");
+        $('#logo-id').css({
+            'width' :'58px', 
+            'left' : '45%',
+            '-webkit-transition' : 'width 2s', /* Safari */
+            'transition' : 'width 2s',
+            display:'block'
+        });
+        
+    }
+    
+    if( $(this).scrollTop() > 10 && $(window).width() >= 768) {
         console.log("Logo Adaptado");
         $('#logo-id').css({
-            'width' :'55px', 
-            
+            'width' :'58px', 
+            'left' : '45%',
             '-webkit-transition' : 'width 2s', /* Safari */
             'transition' : 'width 2s',
             display:'block'
         });
     } 
-      
-    if( $(this).scrollTop() <= 10 ) {
-        console.log("FUNCIONA");
+    
+    if( $(this).scrollTop() <= 10 && $(window).width() >= 768) {
+        console.log("Logo Adaptado Minimizar");
         $('#logo-id').css({
             'width' :'130px', 
             '-webkit-transition' : 'width 2s', /* Safari */
@@ -20,4 +54,31 @@
             display:'block'
         });
     } 
+    
+});
+
+
+$(document).scroll(function() {
+    if( $(this).scrollTop() > 10 && $(window).width() >= 768) {
+        console.log("Logo Adaptado");
+        $('#logo-id').css({
+            'width' :'58px', 
+            'left' : '45%',
+            '-webkit-transition' : 'width 2s', /* Safari */
+            'transition' : 'width 2s',
+            display:'block'
+        });
+    } 
+      
+    if( $(this).scrollTop() <= 10 && $(window).width() >= 768) {
+        console.log("Logo Adaptado Minimizar");
+        $('#logo-id').css({
+            'width' :'130px', 
+            '-webkit-transition' : 'width 2s', /* Safari */
+            'transition' : 'width 2s',
+            display:'block'
+        });
+    } 
+    
+    
 });
