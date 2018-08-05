914d54feba3efa8c27d38b1831d0af4645205c56
ArlisiNoread <41275777+ArlisiNoread@users.noreply.github.com>
2018-08-05 14:01:32 -0500

    Console en redimnesion.js

--- assets/js/redimension.js
+++ assets/js/redimension.js
@@ -1,13 +1,14 @@
 var resta-compensativa = 320;
 
 $(document).ready(function(){
-    102
+    console.log('Redimensión al cargar');
     $('div.jumbotron.carousel-hero').css('padding-top', $(window).height() - resta-compensativa);
     
     console.log($(window).height());
 });
 
 $(window).resize(function(){    
+    console.log('Redimensión al cambiar ventana');
     $('div.jumbotron.carousel-hero').css('padding-top', $(window).height() - resta compensativa);
 });
 
