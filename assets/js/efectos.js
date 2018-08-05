a6e3e6c27db0c073b1fbe77e838f20c60b1988f8
ArlisiNoread <41275777+ArlisiNoread@users.noreply.github.com>
2018-08-05 13:37:51 -0500

    Cambios X

--- assets/js/efectos.js
+++ assets/js/efectos.js
@@ -1,9 +1,10 @@
 //var fixed = false;
   $(document).scroll(function() {
     if( $(this).scrollTop() > 10 ) {
-        console.log("FUNCIONA");
+        console.log("Logo Adaptado");
         $('#logo-id').css({
             'width' :'55px', 
+            
             '-webkit-transition' : 'width 2s', /* Safari */
             'transition' : 'width 2s',
             display:'block'
