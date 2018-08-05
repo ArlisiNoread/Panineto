757b0ecc987fd00471cbe1a3320730101481db6e
ArlisiNoread <41275777+ArlisiNoread@users.noreply.github.com>
2018-08-05 14:33:48 -0500

    Rápido checkeo

--- assets/js/redimension.js
+++ assets/js/redimension.js
@@ -2,13 +2,13 @@ var navbarHeight = $('#barraNavegacion').outerHeight(true);
 
 $(document).ready(function(){
 
-    console.log('Redimensión al cargar');
+    console.log('Redimensión al cargar ' + $(window).height());
     $('div.jumbotron.carousel-hero').css('height', $(window).height() - navbarHeight);
 });
 
 $(window).resize(function(){ 
 
-    console.log('Redimensión al cambiar ventana');
+    console.log('Redimensión al cambiar ventana ' + $(window).height());
     $('div.jumbotron.carousel-hero').css('height', $(window).height() - navbarHeight);
 });
 
