c01003c230d3b587d6c1f58e9c36f5f112e050e5
ArlisiNoread <41275777+ArlisiNoread@users.noreply.github.com>
2018-08-06 00:42:18 -0500

    Efecto Texto

--- assets/js/efectos.js
+++ assets/js/efectos.js
@@ -82,5 +82,25 @@ $(document).scroll(function() {
         });
     } 
     
-    
+});
+
+
+;(function($, win) {
+  $.fn.inViewport = function(cb) {
+     return this.each(function(i,el) {
+       function visPx(){
+         var elH = $(el).outerHeight(),
+             H = $(win).height(),
+             r = el.getBoundingClientRect(), t=r.top, b=r.bottom;
+         return cb.call(el, Math.max(0, t>0? Math.min(elH, H-t) : Math.min(b, H)));  
+       }
+       visPx();
+       $(win).on("resize scroll", visPx);
+     });
+  };
+}(jQuery, window));
+
+
+$(".animation-tracking-in-expand").inViewport(function(px){
+    if(px) $(this).addClass("tracking-in-expand") ;
 });
